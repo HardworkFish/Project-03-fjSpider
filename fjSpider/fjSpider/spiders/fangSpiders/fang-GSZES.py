0d09e9bef54250ebf38a5a6c772cf8e61fdd5f9f
TRsky <625310581@qq.com>
2018-11-01 01:34:02 +0800

    update: fang-GSZES

--- fjSpider/fjSpider/spiders/fangSpiders/fang-GSZES.py
+++ fjSpider/fjSpider/spiders/fangSpiders/fang-GSZES.py
@@ -3,7 +3,7 @@ from re import search
 from urllib.parse import urljoin
 import requests
 from time import sleep
-
+from pyquery import PyQuery as pq
 from bs4 import BeautifulSoup
 from scrapy import FormRequest, Request
 from scrapy.spiders import Spider
@@ -43,21 +43,50 @@ class GSZESSpider(Spider):
     }
 
     def start_requests(self):
-        for i in range(1, 100):
+        for i in range(1, 2):
             # js解析用splash
             yield SplashRequest(self.url % i, callback=self.parse, dont_filter=True)
 
     def parse(self, response):
-        soup = BeautifulSoup(response.body, 'lxml', from_encoding='utf-8')
-        dls = soup.find_all('dl', class_='list rel')
-        for dl in dls:
-            try:
-                item = GDSZES_fangItem()
-                item['url'] = urljoin(response.url, dl.find('a')['href'])
-                item['price_avg'] = int(
-                    dl.find('p', class_='danjia alignR mt5').get_text()[:-3])
-                item['location'] = dl.find(
-                    'span', class_='iconAdress ml10 gray9').get_text()
-                yield item
-            except:
-                continue
+        doc = pq(response.body.decode('utf8'))
+        docs = pq(doc('.shop_list'))
+        #print(docs('float1').items()) 
+        for i in docs('.floatl').items():
+            link = 'http://sz.esf.fang.com'+i('a').attr('href')
+            yield SplashRequest(link, callback=self.second_parse)
+
+    def second_parse(self, response):
+        print(response.url) 
+        item = GFsundeES_fangItem()
+        item['url'] = response.url
+        soup  = BeautifulSoup(response.body, 'lxml')
+        item['title']  = soup.find('h1', class_='title floatl').get_text() 
+        item['price_total'] =  soup.find('div', class_='price_esf').get_text().strip()
+        tts = soup.find_all('div', class_='tt')
+        item['house_type'] = tts[0].get_text().strip()
+        item['house_area'] = tts[1].get_text().strip()
+        item['price_averages'] = tts[2].get_text().strip()
+        item['house_towards'] = tts[3].get_text().strip()
+        item['total_floor'] = soup.find_all('div', class_='trl-item1')[4].find_all(
+            'div')[1].get_text().split('（')[-1].split('）')[0].strip()
+        item['house_floor'] = tts[4].get_text().strip()
+        item['decoration'] = tts[5].get_text().strip()
+      #  print(item)        
+        infos = soup.find_all(
+            'span', class_='rcont')
+       # print(infos[0].get_text().strip())
+        #print(infos)
+        item['house_age'] = infos[0].get_text().strip()
+        item['property_right'] = infos[1].get_text().strip()
+        item['residential_category'] = infos[2].get_text().strip()
+        item['structure'] = infos[3].get_text().strip()
+        item['construction_category'] = infos[4].get_text().strip()
+        item['listed_time'] = infos[5].get_text().strip()
+        #print(item) 
+        item['description'] = soup.find('li', class_='font14 hxmd').get_text().strip()
+        item['contact'] = soup.find(
+            'span', id='mobilecode').get_text().strip()
+        item['belong'] = soup.find(
+            'span', class_='zf_jjname').get_text().strip()
+        print(item)
+       # yield item
